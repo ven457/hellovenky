cd141a74001893ac34026b76f2c0a68821843948
ven457 <48293282+ven457@users.noreply.github.com>
2023-11-30 19:15:54 +0530

    Create sampleai.py

---
+++ sampleai.py
@@ -0,0 +1,7 @@
+import random
+from collections import defaultdict
+from string import punctuations
+
+tokens=["I","try", "to", "learn","something","new","every","day"]
+txt=defaultdict["word"]
+print(txt)
